ef1f59d4863752b5bbdaeafbaa428ee207879624
Sunil H N <sunilh.n27@gmail.com>
2024-02-17 00:06:13 +0530

    Updated Page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Features from "@/components/Features";
 import Features2 from "@/components/Features2";
 import Features3 from "@/components/Features3";
+import GetStarted from "@/components/GetStarted";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import Numbers from "@/components/Numbers";
@@ -8,12 +9,13 @@ import Numbers from "@/components/Numbers";
 export default function Home() {
   return (
     <main>
-      <Navbar />
+ 
       <HeroSection />
       <Features/>
       <Numbers/>
       <Features2/>
       <Features3/>
+      <GetStarted/>
     </main>
   );
 }
